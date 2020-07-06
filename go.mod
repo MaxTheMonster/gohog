b9ab4064cb22e379f5843d1b86be63ad581bf571
James Greenhill <jams@uber.com>
2020-07-05 21:29:19 -0700

    take the key as a param

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.14
 
 require (
 	github.com/google/uuid v1.1.1 // indirect
-	github.com/posthog/posthog-go v0.0.0-20200407154934-8cfe8e524f8b // indirect
+	github.com/posthog/posthog-go v0.0.0-20200407154934-8cfe8e524f8b
 	github.com/xtgo/uuid v0.0.0-20140804021211-a0b114877d4c // indirect
 )
--- main.go
+++ main.go
@@ -12,51 +12,57 @@ import (
 )
 
 const (
-  endpointDefault = "http://localhost:8000"
+	endpointDefault = "http://localhost:8000"
 )
 
 func main() {
-  var endpoint string
-  flag.StringVar(&endpoint, "endpoint", endpointDefault, "Endpoint to generate traffic to")
-  posthogKey := os.Getenv("POSTHOG_KEY") 
-  numbEvents := flag.Int("events", 10000, "Total number of events to spawn")
-  funnelDepth := flag.Int("funnel-depth", 10, "Max depth of funnel")
-  userCount := flag.Int("users", 10000, "user pool to draw from")
-  sleepTime := flag.Int("sleep", 25, "ms to sleep between events")
-  flag.Parse()
- 
-  log.Println("~~~~~~~Beginning burn in test~~~~~~~") 
-  log.Printf("Generating %d events drawing from a pool of %d users\na funnel with a max of %d steps sleeping %d ms between event", *numbEvents, *userCount, *funnelDepth, *sleepTime)
+	var endpoint string
+	flag.StringVar(&endpoint, "endpoint", endpointDefault, "Endpoint to generate traffic to")
+	posthogKey := os.Getenv("POSTHOG_KEY")
+	posthogKeyFlag := flag.String("key", "", "PostHog Api Key")
+	numbEvents := flag.Int("events", 10000, "Total number of events to spawn")
+	funnelDepth := flag.Int("funnel-depth", 10, "Max depth of funnel")
+	userCount := flag.Int("users", 10000, "user pool to draw from")
+	sleepTime := flag.Int("sleep", 25, "ms to sleep between events")
+	flag.Parse()
 
-  config := posthog.Config{
-    Endpoint: endpoint,
-  }
+	if *posthogKeyFlag != "" {
+		posthogKey = *posthogKeyFlag
+	}
 
-  client, err := posthog.NewWithConfig(posthogKey, config)
-  if err != nil {
-    panic("oh no")
-  } 
-  defer client.Close()
+	log.Println("~~~~~~~Beginning burn in test~~~~~~~")
+	log.Printf("Generating %d events drawing from a pool of %d users\na funnel with a max of %d steps sleeping %d ms between event", *numbEvents, *userCount, *funnelDepth, *sleepTime)
 
-  t := 0
-  for t <= *numbEvents {
-    s := rand.Intn(*funnelDepth)
-    userid := rand.Intn(*userCount)
-    user := fmt.Sprintf("user-%d", userid)  
-    plan := "Enterprise"
-    friends := rand.Intn(42) 
-    for i := 0; i <= s ; i++ {
-      t++ 
-      step := fmt.Sprintf("step-%d", i) 
-      log.Printf("Logging total message: %d user: %s step: %s plan: %s friends: %d", t, user, step, plan, friends) 
-      client.Enqueue(posthog.Capture{
-        DistinctId: user,
-        Event:      step,
-        Properties: posthog.NewProperties().
-          Set("plan", plan).
-          Set("friends", friends),
-      })
-      time.Sleep(time.Duration(*sleepTime) * time.Millisecond)
-    } 
-  }
+	config := posthog.Config{
+		Endpoint: endpoint,
+	}
+
+	client, err := posthog.NewWithConfig(posthogKey, config)
+	if err != nil {
+		panic("oh no")
+	}
+	defer client.Close()
+
+	t := 0
+	for t <= *numbEvents {
+		s := rand.Intn(*funnelDepth)
+		userid := rand.Intn(*userCount)
+		user := fmt.Sprintf("user-%d", userid)
+		plan := "Enterprise"
+		friends := rand.Intn(42)
+		for i := 0; i <= s; i++ {
+			t++
+			step := fmt.Sprintf("step-%d", i)
+			log.Printf("Logging total message: %d user: %s step: %s plan: %s friends: %d", t, user, step, plan, friends)
+			client.Enqueue(posthog.Capture{
+				DistinctId: user,
+				Event:      step,
+				// Event: "$pageview",
+				Properties: posthog.NewProperties().
+					Set("plan", plan).
+					Set("friends", friends),
+			})
+			time.Sleep(time.Duration(*sleepTime) * time.Millisecond)
+		}
+	}
 }
